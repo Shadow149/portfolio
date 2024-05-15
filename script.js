8c14700c6b7e419c0f19315a51da59e8733642f9
Alfred Roberts <robertsalfie14@gmail.com>
2024-05-15 19:35:59 +0100

    Add email clipboard copy

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { animate_blob } from "./javascript/blob.js"
 
+let email = "robertsalfie14@gmail.com";
 
 let night_toggle = function(manual = false){ 
     const svgs = Array.from(
@@ -21,6 +22,20 @@ let night_toggle = function(manual = false){
         manual
     );
 };
+
+document.getElementById("emailAnchor").addEventListener("click", function() {
+    document.getElementById("emailSvg").style.display = "none";
+    document.getElementById("tickSvg").style.display = "inline-block";
+    document.getElementById("copyTooltip").style.display = "block";
+
+    navigator.clipboard.writeText(email);
+
+    setTimeout(() => {
+        document.getElementById("emailSvg").style.display = "inline-block";
+        document.getElementById("tickSvg").style.display = "none";
+        document.getElementById("copyTooltip").style.display = "none";
+    }, 1500);
+});
 
 document.getElementById("nightmode").addEventListener("change", function (event) {
     night_toggle(event.target.checked);
